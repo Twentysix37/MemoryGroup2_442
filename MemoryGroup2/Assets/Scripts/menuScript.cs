33ee2a05734b8a32dd1e23abb5c7c7a39c63c68b
RonyParmar <ronyparm@buffalo.edu>
2016-09-17 18:16:41 -0400

    Initial Menu Commit
    First Menu update. Menu assests and scene.

---
+++ MemoryGroup2/Assets/Scripts/menuScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+	public Canvas quitMenu;
+	public Button startText;
+	public Button exitText;
+	public Button optionsText;
+	public Canvas optionsMenu;
+
+	// Use this for initialization
+	void Start () {
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		optionsMenu = optionsMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+		optionsText = optionsText.GetComponent<Button> ();
+		quitMenu.enabled = false;
+		optionsMenu.enabled = false;
+	}
+
+	public void ExitPress(){
+		quitMenu.enabled = true;
+		optionsMenu.enabled = false;
+		optionsText.enabled = false;
+		startText.enabled = false;
+		exitText.enabled = false;
+	}
+	public void OptionsPress(){
+		quitMenu.enabled = false;
+		optionsMenu.enabled = true;
+		optionsText.enabled = false;
+		startText.enabled = false;
+		exitText.enabled = false;
+	}
+
+	public void noPress (){
+		optionsMenu.enabled = false;
+		quitMenu.enabled = false;
+		startText.enabled = true;
+		exitText.enabled = true;
+		optionsText.enabled = true;
+
+	}
+
+	public void Startlevel(){
+		//load a picture
+	}
+
+	public void ExitGame(){
+		Application.Quit ();
+	}
+
+}
