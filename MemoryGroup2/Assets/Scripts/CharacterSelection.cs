9541ae39adb4235ec371bd6e4dbefe94119e5f18
Twentysix37 <Twentysix37@gmail.com>
2016-09-28 18:11:53 -0400

    start of Combat scene
    inital combat commit

---
+++ MemoryGroup2/Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class CharacterSelection : MonoBehaviour {
+
+	public Dictionary<string,List<string>> Characters = new Dictionary<string,List<string>>();
+
+	void FillDictionary (){
+		for (int i = 10; i > 0; i--) {
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		GameObject CharactorSelectButton = new GameObject ();
+		CharactorSelectButton.transform.SetParent();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
