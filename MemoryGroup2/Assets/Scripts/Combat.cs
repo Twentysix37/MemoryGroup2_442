12dbfb3f6e6c943d5545147d4b531c7a624a29cd
Twentysix37 <Twentysix37@gmail.com>
2016-10-09 19:19:54 -0400

    Further work on Combat
    Animations done

--- MemoryGroup2/Assets/Scripts/Combat.cs
+++ MemoryGroup2/Assets/Scripts/Combat.cs
@@ -5,14 +5,56 @@ using System.Collections;
 public class Combat : MonoBehaviour {
 	public Text ResultText;
 	public int PlayerValue;
+	Image backgroundSprite;
+	public SpriteRenderer Player;
+	public SpriteRenderer Enemy;
+	Animator playerani;
+	Animator enemyani;
 
 	public void CombatResult (int PlayerValue){
-		if (PlayerValue > 0){//EnemyValue) {
-			ResultText.text = "Win!";
-		} else if (PlayerValue < 0){ //EnemyValue) {
-			ResultText.text = "Lose!";
+		//enemyani.SetBool ("Ani", false);
+		//playerani.SetBool ("Ani", false);
+		backgroundSprite = ResultText.GetComponentInParent<Image> ();
+		if (PlayerValue > 0) {//EnemyValue) {
+			playerani = Player.GetComponent<Animator> ();
+			playerani.SetBool ("Ani", true);
+
+			StartCoroutine( Waiting ("Win!"));
+
+			//backgroundSprite.enabled = false;
+
+
+		} else if (PlayerValue < 0) { //EnemyValue) {
+			enemyani = Enemy.GetComponent<Animator> ();
+			enemyani.SetBool ("Ani", true);
+
+			StartCoroutine( Waiting ("Lose!"));
+			//backgroundSprite.enabled = false;
+
+
+
 		} else {
-			ResultText.text = "Draw...";
+			playerani = Player.GetComponent<Animator> ();
+			playerani.SetBool ("Ani", true);
+
+			enemyani = Enemy.GetComponent<Animator> ();
+			enemyani.SetBool ("Ani", true);
+
+			StartCoroutine( Waiting ("Draw..."));
+			//backgroundSprite.enabled = false;
+
+
 		}
 	}
-}
+
+	IEnumerator Waiting (string text){
+		yield return new WaitForSecondsRealtime(1.5f);
+		ResultText.text = text;
+
+		backgroundSprite.enabled = true;
+		playerani.SetBool ("Ani", false);
+		enemyani.SetBool ("Ani", false);
+
+		}
+	}
+
