d76ccfca60d3c42c54e61e7009f23e8a74a32e79
Twentysix37 <Twentysix37@gmail.com>
2016-11-08 16:15:59 -0500

    Combat script; combat evaluation function changes

--- MemoryGroup2/Assets/Scripts/Combat.cs
+++ MemoryGroup2/Assets/Scripts/Combat.cs
@@ -24,20 +24,53 @@ public class Combat : MonoBehaviour {
 	public Button Back;
 	int counter = 0;
 	List<string> Names = new List<string>();
+	List<string> Values = new List<string>();
+	List<string> cpuNames = new List<string>();
+	List<string> cpuValues = new List<string>();
 
 	void Start(){
 		GameObject selectionCanvas = GameObject.Find ("Player Selection Canvas1");
 		selectionCanvas.SetActive(false);
 		selectionScript2 selScript = selectionCanvas.GetComponent <selectionScript2> ();
-		List<string> playerNames = new List<string>();
+		List<string> playerNames = new List<string> ();
+		List<string> playerValues = new List<string> ();
+		List<string> enemyNames = new List<string> ();
+		List<string> enemyValues = new List<string> ();
+
 		var keyCollection = selScript.playerDict.Keys;
 		foreach (var key in keyCollection) {
 			playerNames.Add (key);
 		}
 
+		var valueCollection = selScript.playerDict.Values;
+		foreach (var val in valueCollection) {
+			playerValues.Add (val);
+		}
+
+		var keyEnemyCollection = selScript.computerDict.Keys;
+		foreach (var key in keyCollection) {
+			enemyNames.Add (key);
+		}
+
+		var valueEnemyCollection = selScript.computerDict.Values;
+		foreach (var val in valueCollection) {
+			enemyValues.Add (val);
+		}
+
 		Names.Add (playerNames[0]);
 		Names.Add (playerNames[1]);
 		Names.Add (playerNames[2]);
+		cpuNames.Add (enemyNames [0]);
+		cpuNames.Add(enemyNames[1]);
+		cpuNames.Add(enemyNames[2]);
+
+		Values.Add (playerValues [0]);
+		Values.Add (playerValues [1]);
+		Values.Add (playerValues [2]);
+		cpuValues.Add(enemyValues [0]);
+		cpuValues.Add(enemyValues [1]);
+		cpuValues.Add(enemyValues [2]);
+
 		Button1.GetComponentInChildren<Text>().text = Names[0];
 		Button2.GetComponentInChildren<Text>().text = Names[1];
 		Button3.GetComponentInChildren<Text>().text = Names[2];
@@ -47,12 +80,12 @@ public class Combat : MonoBehaviour {
 		
 		Player.SetActive(false);
 		Enemy.SetActive (false);
-		EnemyValue = 0;
+
 
 		//enemyani.SetBool ("Ani", false);
 		//playerani.SetBool ("Ani", false);
 		backgroundSprite = ResultText.GetComponentInParent<Image> ();
-		if (PlayerValue > EnemyValue) {//EnemyValue) {
+		if ((Values[PlayerValue] + cpuValues[counter]) <= 800) {//EnemyValue) {
 			counter++;
 			
 			Player = GameObject.Instantiate (Resources.Load ("Prefab/KnightAttack_0", typeof(GameObject))) as GameObject;
@@ -69,7 +102,7 @@ public class Combat : MonoBehaviour {
 			//backgroundSprite.enabled = false;
 
 
-		} else if (PlayerValue < EnemyValue) { //EnemyValue) {
+		} else if ((Values[PlayerValue] + cpuValues[counter]) >= 1300) { //EnemyValue) {
 			counter++;
 			Player = GameObject.Instantiate (Resources.Load ("Prefab/KnightAttack_0", typeof(GameObject))) as GameObject;
 			playerSpriteRenderer = Player.GetComponent<SpriteRenderer>();
